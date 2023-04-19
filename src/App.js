867cbddd062e907fc707193392ce8ba01a7b6182
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-19 14:41:29 +0600

    contact route defined

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
 
             <Route path="/home" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
 
           </Routes>     
         </BrowserRouter>
