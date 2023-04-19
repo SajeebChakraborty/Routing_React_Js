d7a7eddc5b44fb870f21920060ed930534f4cf52
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-19 14:43:06 +0600

    blogs component defined

---
+++ src/components/Blogs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <div>
+        
+      <h1>Blogs Page</h1>
+      <p>This a blogs page</p>
+      
+    </div>
+  )
+}
