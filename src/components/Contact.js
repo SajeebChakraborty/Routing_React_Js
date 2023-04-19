6713028e0ecf718f727a3da7b2359a8bb7485a29
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-19 15:21:59 +0600

    redirect by navigate hook

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Contact() {
+
+  const navigate = useNavigate()
+
   return (
     <div>
 
       <h1>Contact Page</h1>
     <p>This a contact page</p>
-    
+
+    <button onClick={()=> {navigate("/")}}>Go to back home page</button>
+
     </div>
   )
 }
