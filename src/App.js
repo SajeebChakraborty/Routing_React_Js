02f7077919698d80564d82730f7aa843cbf0a4ab
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-19 14:39:30 +0600

    home route defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div  >
+
+        <BrowserRouter>
+          <Routes>
+
+            <Route path="/home" element={<Home />} />
+
+          </Routes>     
+        </BrowserRouter>
       
     </div>
   );
