1ed5e9c9e8ab12e0fbc8e0bae0bd23a303a69be2
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-19 15:25:31 +0600

    dummy data added

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const blogsData = [
+
+    {
+        id: 1,
+        title: 'Blog 1',
+        content: 'Blog 1 content'
+    },
+
+    {
+        id: 2,
+        title: 'Blog 2',
+        content: 'Blog 2 content'
+    },
+    {
+        id: 3,
+        title: 'Blog 3',
+        content: 'Blog 3 content'
+    },
+    {
+        id: 4,
+        title: 'Blog 4',
+        content: 'Blog 4 content'
+    },
+    {
+        id: 5,
+        title: 'Blog 5',
+        content: 'Blog 5 content'
+    }
+
+    
+];
